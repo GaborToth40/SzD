c32382b6ff77432ad9928843f595a4db04416373
GaborToth40 <tothgabor658@gmail.com>
2026-02-02 15:32:36 +0100

    Added table creation

--- database.py
+++ database.py
@@ -4,3 +4,25 @@ connection = sqlite3.connect('database.db')
 
 cursor = connection.cursor()
 
+command1 = """CREATE TABLE IF NOT EXISTS "cards" (
+    "card_id"    INTEGER PRIMARY KEY AUTOINCREMENT,
+"question"   TEXT,
+"answer"     TEXT,
+"note"       TEXT,
+"weight"     REAL,
+"deck_id"    INTEGER,
+FOREIGN KEY ("deck_id") REFERENCES "decks"("deck_id")
+)"""
+
+cursor.execute(command1)
+
+
+command2 = """CREATE TABLE IF NOT EXISTS "decks" (
+    "deck_id"       INTEGER,
+    "name"          TEXT,
+    "description"   TEXT,
+    "cards_number"  INTEGER,
+    PRIMARY KEY("deck_id" AUTOINCREMENT)
+)"""
+
+cursor.execute(command2)
