58f03df7ca47832a2c9ef9cb70cc85f1f95b1183
GaborToth40 <tothgabor658@gmail.com>
2026-02-02 17:09:09 +0100

    Added GUI elements

--- cards_window.py
+++ cards_window.py
@@ -1,5 +1,33 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QTableWidgetItem, QPushButton)
+from database import connection, cursor
 
 class CardsWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Cards")
+        self.resize(800, 800)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(7)
+        self.table.setHorizontalHeaderLabels(["Select", "ID", "Question", "Answer", "Note", "Weight", "Deck"])
+        layout.addWidget(self.table)
+
+        buttons_layout = QHBoxLayout()
+        layout.addLayout(buttons_layout)
+
+        self.save_button = QPushButton("Save")
+        #self.save_button.clicked.connect(self.save_clicked)
+        buttons_layout.addWidget(self.save_button)
+
+        self.delete_button = QPushButton("Delete")
+        #self.delete_button.clicked.connect(self.delete_selected_rows)
+        buttons_layout.addWidget(self.delete_button)
+
+        #self.decks = self.load_decks()
+
+        #self.load_cards()
+        #self.add_empty_row
